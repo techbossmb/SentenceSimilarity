ebd5d483b47060ade7a54d5f2257dc7b89f861a4
Ishola Babatunde Isaac <isholabi@gmail.com>
2019-02-18 10:23:53 -0600

    Update test_similarity.py

--- test_similarity.py
+++ test_similarity.py
@@ -18,72 +18,119 @@ import loadfaqs as faq
 @date: 08/29/17
 '''
 
-def get_faqs(tokenizer, opts):
-	'''
-	get_faqs loads all faq files in data/faq dir into a single list 
-	@argu
-		tokenizer: saved tokenizer object
-		opts: configuration params
-	@return list of faq sentence
-	'''
-	faqs = faq.load_faqs(opts.faq_dir)
-	faqs = [utils.clean_str(sentence) for sentence in faqs]
-	faqs_seq = tokenizer.texts_to_sequences(faqs)
-	faqs_data = pad_sequences(faqs_seq, opts.max_sequence_len)
-	print 'loaded faqs'
-	return faqs_data
+class SimilarityHelper:
 
-def user_request(tokenizer, opts, faq_size)
-	'''
-	user_request retrieves user query
-	@argu
-		tokenizer: saved tokenizer object
-		opts: configuration params
-		faq_size: len of the loaded faq list
-	@return list with repeated user input 
-	'''
-	user_input = raw_input('Enter your question: ')
+	def __init__(self):
+		self.opts = self.programParser()
+		self.tokenizer = pickle.load(open(self.opts.token_object, 'rb'))
+		self.model = self.loadModel()
+		self.faqs_data, self.faqs = self.get_faqs(self.tokenizer, self.opts)
+		self.get_faqs(self.tokenizer, self.opts)
+
+		print "Similarity model ready to roll"
+
+	def get_faqs(self, tokenizer, opts):
+
+		'''
+		get_faqs loads all faq files in data/faq dir into a single list 
+		@argu
+			tokenizer: saved tokenizer object
+			opts: configuration params
+		@returns
+			faqs_data: list of faq sentence
+			faqs: list of faq (sentence list)
+		'''
+
+		faqs = faq.load_faqs(opts.faq_dir)
+		clean_faqs = [utils.clean_str(sentence) for sentence in faqs]
+		faqs_seq = tokenizer.texts_to_sequences(clean_faqs)
+		faqs_data = pad_sequences(faqs_seq, opts.max_sequence_len)
+
+		print 'loaded faqs'
+		return faqs_data, faqs
+
+	def get_answers(self, text):
+
+		'''
+		get_answers loop through faq files in data/faq dir 
+		@returns
+			str: response to user's question
+		'''
+
+		prediction_result = self.predictSimilarity(text)
+		question = max(prediction_result.iteritems(), key=lambda x: x[1])[0]
+		answer = faq.load_answers(self.opts.faq_dir, question)
+		print "loaded answers"
+		return answer
+
+	def user_request(self, tokenizer, text,  opts, faq_size):
+		'''
+		user_request retrieves user query
+		modify as fit - make sure each user question is repeated n times
+		(where n= len(faqs)). the idea is to compare the user request 
+		to every single faq
+		@argu
+			tokenizer: saved tokenizer object
+			opts: configuration params
+			faq_size: len of the loaded faq list
+		@returns 
+			user_input_list_data: list with repeated user input 
+			user_input: question asked by user  to be matched to known faqs
+		'''
+
+		user_input = text
+
 	# note: make a list of repeated user input same size as faq
-	user_input_list = [user_input for i in range(faq_size)]
-	user_input_list = [utils.clean_str(sentence) for sentence in user_input_list]
-	user_input_list_seq = tokenizer.texts_to_sequences(user_input_list)
-	user_input_list_data = pad_sequences(user_input_list_seq, opts.max_sequence_len)
-	return user_input_list_data
-	
-def main():
-	# load program options
-	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	parser.add_argument('--faq_dir', type=str, default='../data/faq', help='dir containing the faqs')
-	parser.add_argument('--max_sequence_len', type=int, default=20, help='maximum number of words allowable in query')
-	parser.add_argument('--model_file', type=str, default='../data/bilstm_sparse_model.h5', help='saved sentence similarity model')
-	parser.add_argument('--weights_file', type=str, default='../data/bilstm_sparse_weights.h5', help='saved model weight')
-	parser.add_argument('--token_object', type=str, default='../data/tokenizer', help='saved tokenizer object file')
-	opts = parser.parse_args()
-	
-	np.random.seed(1)
-	
-	tokenizer = pickle.load(open(opts.token_object, 'rb'))
-	
-	# load default faqs
-	faqs_data = get_faqs(tokenizer, opts)
-	
-	# load user input
-	request = user_request(tokenizer, opts, len(faqs_data))
-	
-	# load saved model
-	model = load_model(opts.model_file)
-	model.load_weights(opts.weights_file)
-	model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-	print 'trained sentence similarity model loaded from file'
+		user_input_list = [user_input for i in range(faq_size)]
+		user_input_list = [utils.clean_str(sentence) for sentence in user_input_list]
+		user_input_list_seq = tokenizer.texts_to_sequences(user_input_list)
+		user_input_list_data = pad_sequences(user_input_list_seq, opts.max_sequence_len)
+		return user_input_list_data, user_input
 
-	# predict sentence similarity
-	prediction = model.predict([faqs_data, request])
-	predicted_classes = np.argmax(prediction, axis=1)
+	def programParser(self):
 
-	match_indexes = [i for i, m in enumerate(predicted_classes) if m==1]
-	for i in range(len(match_indexes)):
-		print questions[match_indexes[i]], ' -> p('+prediction[match_indexes[i]][1]+')'
+		parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+		parser.add_argument('--faq_dir', type=str, default='../data/faq', help='dir containing the faqs')
+		parser.add_argument('--max_sequence_len', type=int, default=20, help='maximum number of words allowable in query')
+		parser.add_argument('--model_file', type=str, default='../data/bilstm_sparse_model.h5', help='saved sentence similarity model')
+		parser.add_argument('--weights_file', type=str, default='../data/bilstm_sparse_weights.h5', help='saved model weight')
+		parser.add_argument('--token_object', type=str, default='../data/tokenizer', help='saved tokenizer object file')
+		opts = parser.parse_args()
 
-if __name__=='__main__':
-	main()
-	print 'done'
+		return opts
+
+	def loadModel(self):
+
+		np.random.seed(1)
+
+		# load saved model
+		model = load_model(self.opts.model_file)
+		model.load_weights(self.opts.weights_file)
+		model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+		print 'trained sentence similarity model loaded from file'
+
+		return model
+
+
+
+	def predictSimilarity(self, text):
+
+		np.random.seed(1)
+		predicted_output = {}
+
+		# load user input
+		[request, user_input] = self.user_request(self.tokenizer, text, self.opts, len(self.faqs_data))
+
+		# predict sentence similarity
+		prediction = self.model.predict([self.faqs_data, request])
+		predicted_classes = np.argmax(prediction, axis=1)
+
+		print 'question: ', user_input
+		print 'see match(es) below'
+		match_indexes = [i for i, m in enumerate(predicted_classes) if m==1]
+
+		for i in range(len(match_indexes)):
+			predicted_output[self.faqs[match_indexes[i]]] = str(prediction[match_indexes[i]][1])
+
+		return predicted_output
+
